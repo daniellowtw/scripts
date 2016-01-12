6cba97fc47f67d1eafcf37a366e57db278b917b9
Daniel Low <daniellowtw@gmail.com>
2016-01-12 13:30:01 +0000

    Update set-up-github-alias.sh

--- set-up-github-alias.sh
+++ set-up-github-alias.sh
@@ -1,6 +1,7 @@
+#!/bin/sh
 git config --global alias.st status
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci 'commit -v'
-git config --global alias.unstage reset HEAD --
-git config --global alias.last log -1 HEAD
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
