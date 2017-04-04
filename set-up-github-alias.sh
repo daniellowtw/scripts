f1298c3daba805b96fc4f2c5274e9ed3458acd3e
Daniel Low <daniel@improbable.io>
2017-04-04 14:16:17 +0100

    Add git recent

--- set-up-github-alias.sh
+++ set-up-github-alias.sh
@@ -5,3 +5,4 @@ git config --global alias.br branch
 git config --global alias.ci 'commit -v'
 git config --global alias.unstage 'reset HEAD --'
 git config --global alias.last 'log -1 HEAD'
+git config --global alias.recent "for-each-ref --sort=committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)'"
