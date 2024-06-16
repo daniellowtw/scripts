06630668056af83caa191cde139c7d8c946f7cf7
Daniel Low <daniellowtw@gmail.com>
2024-06-16 18:10:46 +0100

    fix: update linux script for 2024

--- dotfiles/set-up-rc.sh
+++ dotfiles/set-up-rc.sh
@@ -7,22 +7,22 @@ source ./lib.sh
 warn
 # make blank localdlowrc if it doesn't exist
 if [[ ! -e ~/.localdlowrc ]]; then
-  echo "creating localdlowrc for local stuff"
-  cp .localdlowrc ~/.localdlowrc
+	echo "creating localdlowrc for local stuff"
+	cp .localdlowrc ~/.localdlowrc
 fi
 
-if [[ ! -e ~/bin ]]; then 
+if [[ ! -e ~/bin ]]; then
 
-echo "Set up local bin"
-mkdir -p ~/bin
+	echo "Set up local bin"
+	mkdir -p ~/bin
 fi
 
 if [[ -e ~/.zshrc ]]; then
-  echo "zshrc detected"
-  symlink ".zshrc"
-  echo "source ~/.localdlowrc" >> ~/.zshrc
+	echo "zshrc detected"
+	symlink ".zshrc"
+	echo "source ~/.localdlowrc" >>~/.zshrc
 else
-  echo "No zshrc, copying to bashrc"
-  echo "source ~/.localdlowrc" >> ~/.bashrc
+	echo "No zshrc, copying to bashrc"
+	echo "source ~/.localdlowrc" >>~/.bashrc
 fi
 echo "Loaded zshrc. Please restart shell."
--- linux/setup.sh
+++ linux/setup.sh
@@ -4,36 +4,81 @@ set -eux -o pipefail
 
 # Interactive session to set up software
 sudo apt update
-sudo apt install -y rg curl rg yq gh build-essential git fzf vim htop zsh
+sudo apt install -y ripgrep curl jq gh build-essential git fzf vim htop zsh
+touch ~/.zshrc
+sudo chsh -s $(which zsh)
+
+if [[ $(command -v yq) ]]; then
+	echo "Bat is already installed"
+else
+	VERSION=v4.2.0
+	BINARY=yq_linux_amd64
+	wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |
+		tar xz &&
+		sudo mv ${BINARY} /usr/bin/yq
+fi
+
+if [[ $(command -v nvim) ]]; then
+  echo "nvim is already installed"
+else
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+fi
+
 
 # Create key
 # ssh-keygen -t ed25519 -b 4096
 # gh auth login
 
-
 # Install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+if [[ $(command -v rustup) ]]; then
+	echo "Rust is already installed"
+else
+	echo "Installing Rust"
+  # Interactive
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  $HOME/.local/bin/cargo install zellij
+fi
 
-cargo install ripgrep zellij
 
 # Python
 
-# https://github.com/conda-forge/miniforge#mambaforge
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-bash Mambaforge-$(uname)-$(uname -m).sh
+if [[ $(command -v conda) ]]; then
+  echo "Conda is already installed"
+else
+  # https://github.com/conda-forge/miniforge#mambaforge
+  curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+  bash Miniforge3-$(uname)-$(uname -m).sh
+  rm Miniforge3-$(uname)-$(uname -m).sh
+fi
+
 
 # Install go
-curl -L -O https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+if [[ $(command -v go) ]]; then
+  echo "Go is already installed"
+else
+  echo "Installing Go"
+  curl -L -O https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
+  sudo rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.4.linux-amd64.tar.gz
+fi
 
-# Install zsh and go
-touch ~/.zshrc
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  echo "Oh my zsh is already installed"
+else
+  sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+fi
 
+if [[ $(command -v docker) ]]; then
+  echo "Docker is already installed"
+else
 # Install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh --dry-run
-echo "Does it look ok? Ctrl + C to exit if not"
-read
-sudo sh ./get-docker.sh 
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh ./get-docker.sh --dry-run
+  echo "Does it look ok? Ctrl + C to exit if not"
+  read
+  sudo sh ./get-docker.sh
+  sudo usermod -aG docker $USER
+  rm get-docker.sh
+fi
