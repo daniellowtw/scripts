583e9bf2d7bef168abf7640eb15e9792002f3d47
Daniel Low <daniellowtw@gmail.com>
2025-04-21 00:30:23 +0100

    feat: remove ~/bin
    Use OS defaults like using ~/.local/bin

--- dotfiles/set-up-rc.sh
+++ dotfiles/set-up-rc.sh
@@ -7,22 +7,16 @@ source ./lib.sh
 warn
 # make blank localdlowrc if it doesn't exist
 if [[ ! -e ~/.localdlowrc ]]; then
-	echo "creating localdlowrc for local stuff"
-	cp .localdlowrc ~/.localdlowrc
-fi
-
-if [[ ! -e ~/bin ]]; then
-
-	echo "Set up local bin"
-	mkdir -p ~/bin
+  echo "creating localdlowrc for local stuff"
+  cp .localdlowrc ~/.localdlowrc
 fi
 
 if [[ -e ~/.zshrc ]]; then
-	echo "zshrc detected"
-	symlink ".zshrc"
-	echo "source ~/.localdlowrc" >>~/.zshrc
+  echo "zshrc detected"
+  symlink ".zshrc"
+  echo "source ~/.localdlowrc" >>~/.zshrc
 else
-	echo "No zshrc, copying to bashrc"
-	echo "source ~/.localdlowrc" >>~/.bashrc
+  echo "No zshrc, copying to bashrc"
+  echo "source ~/.localdlowrc" >>~/.bashrc
 fi
 echo "Loaded zshrc. Please restart shell."
