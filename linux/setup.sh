c1097b4b240388260700fd828b5ad6cf53ca6502
Daniel Low <daniellowtw@gmail.com>
2026-02-15 00:19:11 +0000

    fix: nvim

--- linux/setup.sh
+++ linux/setup.sh
@@ -21,10 +21,10 @@ fi
 if [[ $(command -v nvim) ]]; then
   echo "nvim is already installed"
 else
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+  curl -LO https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-linux-x86_64.tar.gz
   sudo rm -rf /opt/nvim
-  sudo tar -C /opt -xzf nvim-linux64.tar.gz
-  rm nvim-linux64.tar.gz
+  sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+  rm nvim-linux-x86_64.tar.gz
 fi
 
 # Create key
